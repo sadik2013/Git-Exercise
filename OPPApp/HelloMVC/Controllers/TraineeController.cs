fa0386f18de5d7194f8359d7c54a111698136c7d
sadikaiub <rafiqulislamsadik55@gmail.com>
2019-02-18 17:11:16 +0600

    Add MVC project and model binding

---
+++ OPPApp/HelloMVC/Controllers/TraineeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class TraineeController : Controller
+    {
+        public string Entry(Trainee model)
+        {
+            return "Name:" + model.Name + " " + "Reg No:" + model.RegNo;
+        }
+
+    }
+}
---
+++ OPPApp/HelloMVC/Models/Trainee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMVC.Models
+{
+    public class Trainee
+    {
+        public string Name { get; set; }
+        public string RegNo { get; set; }
+    }
+}
